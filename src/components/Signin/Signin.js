3ad7eadaccf6ba7eb9c5ad783ca9ccd5f58a5dac
Milena Davydova <milena@yelk.com.ua>
2022-10-13 23:06:50 +0200

    refactoring

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('https://damp-brushlands-40839.herokuapp.com//signin', {
+        fetch('https://damp-brushlands-40839.herokuapp.com/signin', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
